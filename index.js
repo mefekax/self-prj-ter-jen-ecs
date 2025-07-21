9da599e2c2c07feca311da4c410aac9626ccfb64
mefekax <mefeka@yahoo.com>
2025-07-21 22:31:08 +0200

    changes in index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var express=require('express');
 var app=express();
 app.get('/',function (req,res)
 {
-    res.send('Hello World!');
+    res.send('Hello World! This is a simple Express app. You can access this app at http://localhost:3000/ or http://127.0.0.1:3000/ . Feel free to modify the code and explore more features of Express.js!');
 });
 var server=app.listen(3000,function() {
     var host=server.address().address;
